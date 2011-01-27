97603430c874be2644a41de72747940db46b95af
Ben Karel <eschew@gmail.com>
2011-01-27 02:32:37 -0500

    Update console wrappers, add a function to strip console-related commands from an Output, yielding a String.

--- compiler/me/src/Foster/Base.hs
+++ compiler/me/src/Foster/Base.hs
@@ -3,22 +3,28 @@ module Foster.Base where
 import System.Console.ANSI
 import Control.Monad
 
-data Outputs = Outputs [SGR] String
-type Output = [Outputs]
+data OutputData = OutputData { outputDataSGRs :: [SGR]
+                             , outputDataString :: String }
+type Output = [OutputData]
+
+instance Eq OutputData where
+    (OutputData _ sa) == (OutputData _ sb) = sa == sb
 
 out :: String -> Output
-out s = [(Outputs ([] :: [SGR]) s)]
+out s = [(OutputData ([] :: [SGR]) s)]
 
 outLn s = out (s ++ "\n")
 
 outCS :: Color -> String -> Output
-outCS c s = [Outputs [SetColor Foreground Dull c] s]
+outCS c s = [OutputData [SetColor Foreground Dull c] s]
 
 outCSLn c s = outCS c (s ++ "\n")
 
+outToString o = fmap outputDataString o
+
 runOutput :: Output -> IO ()
 runOutput outs = do
-    forM_ outs $ (\(Outputs sgrs str) -> do
+    forM_ outs $ (\(OutputData sgrs str) -> do
                 _ <- setSGR sgrs
                 putStr str
                 setSGR []
@@ -36,3 +42,5 @@ instance Show Ident where
     show i = (identPrefix i) ++ "!" ++ (show $ identNum i)
 
 irrelevantIdentNum = (-12345) :: Int
+
+
