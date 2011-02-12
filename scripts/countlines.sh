c073cd6a952ae6ca5c8463793288507acf92407e
Ben Karel <eschew@gmail.com>
2011-02-12 06:56:07 -0500

    Add a script to summarize line counts.

---
+++ scripts/countlines.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+title () {
+  echo ""
+  echo ""
+  echo "****************************************************"
+  echo "             " $@
+  echo "****************************************************"
+}
+
+sumof () {
+  $@ | grep SUM | awk '{print $5}'
+}
+
+insp () {
+  echo -n "$1" ' '
+  shift
+  sumof cloc $@ --quiet --skip-uniqueness --exclude-dir=compiler/me/src/Foster/Fepb,compiler/me/src/Foster/Bepb
+}
+
+need () {
+  if [ "q" = "q`which $1`" ]; then
+    echo "Must have $1 in PATH"
+    exit
+  fi
+}
+
+need cloc
+need grep
+need awk
+
+# title "runtime"
+# cloc --quiet runtime --by-file
+
+echo -n "notes            "
+cat   notes/*.rst notes/*.txt | wc -l
+echo
+
+insp 'compiler/*.cpp ' compiler/*.cpp
+insp "compiler/passes" compiler/passes compiler/include/foster/passes
+insp "compiler/parse " compiler/parse  compiler/include/foster/parse
+insp "compiler/base  " compiler/parse  compiler/include/foster/base
+insp "compiler/llvm  " compiler/llvm
+insp "compiler/me    " compiler/me
+echo
+insp "runtime        " runtime
+insp "compiler       " compiler
+insp "third_party    " third_party
