e6eb8c0f61c287035092ed0585b8b2670fa80efc
Ben Karel <eschew@gmail.com>
2011-02-18 21:31:04 -0500

    Move Structured instance declarations below ExprAST and AnnExpr definitions.

--- compiler/me/src/Foster/ExprAST.hs
+++ compiler/me/src/Foster/ExprAST.hs
@@ -23,92 +23,6 @@ data CompilesStatus = CS_WouldCompile | CS_WouldNotCompile | CS_NotChecked
 class Expr a where
     freeVars   :: a -> [String]
 
-tryGetCallNameE :: ExprAST -> String
-tryGetCallNameE (E_VarAST mt v) = v
-tryGetCallNameE _ = ""
-
-instance Structured ExprAST where
-    textOf e width =
-        let spaces = Prelude.replicate width '\SP'  in
-        case e of
-            E_BoolAST rng  b     -> out $ "BoolAST      " ++ (show b)
-            E_CallAST rng call   -> out $ "CallAST      " ++ tryGetCallNameE (callASTbase call)
-            E_CompilesAST e c    -> out $ "CompilesAST  "
-            E_IfAST _            -> out $ "IfAST        "
-            E_IntAST rng text    -> out $ "IntAST       " ++ text
-            E_FnAST f            -> out $ "FnAST        " ++ (prototypeASTname $ fnProto f)
-            E_SeqAST   a b       -> out $ "SeqAST       "
-            E_SubscriptAST  a b  -> out $ "SubscriptAST "
-            E_TupleAST     es b  -> out $ "TupleAST     "
-            E_VarAST mt v        -> out $ "VarAST       " ++ v ++ " :: " ++ show mt
-    childrenOf e =
-        case e of
-            E_BoolAST rng b      -> []
-            E_CallAST rng call   -> [callASTbase call, callASTargs call]
-            E_CompilesAST   e c  -> [e]
-            E_IfAST (IfAST a b c)-> [a, b, c]
-            E_IntAST rng txt     -> []
-            E_FnAST f            -> [fnBody f]
-            E_SeqAST        a b  -> unbuildSeqs e
-            E_SubscriptAST  a b  -> [a, b]
-            E_TupleAST     es b  -> es
-            E_VarAST       mt v  -> []
-
-instance Expr ExprAST where
-    freeVars e = case e of
-        E_VarAST mt nm      -> [nm]
-        E_FnAST f           -> let bodyvars =  Set.fromList (freeVars (fnBody f)) in
-                               let boundvars = Set.fromList (map (identPrefix.avarIdent) (prototypeASTformals (fnProto f))) in
-                               Set.toList (Set.difference bodyvars boundvars)
-        _                   -> concatMap freeVars (childrenOf e)
-
-
-instance Structured AnnExpr where
-    textOf e width =
-        let spaces = Prelude.replicate width '\SP'  in
-        case e of
-            AnnBool         b    -> out $ "AnnBool      " ++ (show b)
-            AnnCall  r t b a     -> out $ "AnnCall      " ++ " :: " ++ show t
-            AnnCompiles c msg    -> out $ "AnnCompiles  " ++ show c ++ " - " ++ msg
-            AnnIf      t  a b c  -> out $ "AnnIf        " ++ " :: " ++ show t
-            AnnInt ty int        -> out $ "AnnInt       " ++ (litIntText int) ++ " :: " ++ show ty
-            E_AnnFn annFn        -> out $ "AnnFn " ++ fnNameA annFn ++ " // " ++ (show $ annFnBoundNames annFn)
-            AnnSeq          a b  -> out $ "AnnSeq       " ++ " :: " ++ show (typeAST b)
-            AnnSubscript  t a b  -> out $ "AnnSubscript " ++ " :: " ++ show t
-            AnnTuple     es b    -> out $ "AnnTuple     "
-            E_AnnVar (AnnVar t v) -> out $ "AnnVar       " ++ show v ++ " :: " ++ show t
-            E_AnnTyApp t e argty -> out $ "AnnTyApp     [" ++ show argty ++ "] :: " ++ show t
-    childrenOf e =
-        case e of
-            AnnBool         b                    -> []
-            AnnCall  r t b a                     -> [b, a]
-            AnnCompiles c msg                    -> []
-            AnnIf      t  a b c                  -> [a, b, c]
-            AnnInt t _                           -> []
-            E_AnnFn annFn                        -> [annFnBody annFn]
-            AnnSeq      a b                      -> unbuildSeqsA e
-            AnnSubscript t a b                   -> [a, b]
-            AnnTuple     es b                    -> es
-            E_AnnVar      v                      -> []
-            E_AnnTyApp t e argty                 -> [e]
-
-instance Expr AnnExpr where
-    freeVars e = [identPrefix i | i <- freeIdentsA e]
-
-freeIdentsA e = case e of
-        E_AnnVar v      -> [avarIdent v]
-        E_AnnFn f       -> let bodyvars =  freeIdentsA (annFnBody f) in
-                           let boundvars = map avarIdent (annProtoVars (annFnProto f)) in
-                           bodyvars `butnot` boundvars
-        _               -> concatMap freeIdentsA (childrenOf e)
-
-
-annFnBoundNames :: AnnFn -> [String]
-annFnBoundNames fn =
-    let vars = annProtoVars (annFnProto fn) in
-    map show vars
-
-
 data CallAST = CallAST { callASTbase :: ExprAST
                        , callASTargs :: ExprAST
                        } deriving (Show)
@@ -229,4 +143,89 @@ typeAST (E_AnnTyApp substitutedTy tm tyArgs) = substitutedTy
 
 -----------------------------------------------------------------------
 
+
+tryGetCallNameE :: ExprAST -> String
+tryGetCallNameE (E_VarAST mt v) = v
+tryGetCallNameE _ = ""
+
+instance Structured ExprAST where
+    textOf e width =
+        let spaces = Prelude.replicate width '\SP'  in
+        case e of
+            E_BoolAST rng  b     -> out $ "BoolAST      " ++ (show b)
+            E_CallAST rng call   -> out $ "CallAST      " ++ tryGetCallNameE (callASTbase call)
+            E_CompilesAST e c    -> out $ "CompilesAST  "
+            E_IfAST _            -> out $ "IfAST        "
+            E_IntAST rng text    -> out $ "IntAST       " ++ text
+            E_FnAST f            -> out $ "FnAST        " ++ (prototypeASTname $ fnProto f)
+            E_SeqAST   a b       -> out $ "SeqAST       "
+            E_SubscriptAST  a b  -> out $ "SubscriptAST "
+            E_TupleAST     es b  -> out $ "TupleAST     "
+            E_VarAST mt v        -> out $ "VarAST       " ++ v ++ " :: " ++ show mt
+    childrenOf e =
+        case e of
+            E_BoolAST rng b      -> []
+            E_CallAST rng call   -> [callASTbase call, callASTargs call]
+            E_CompilesAST   e c  -> [e]
+            E_IfAST (IfAST a b c)-> [a, b, c]
+            E_IntAST rng txt     -> []
+            E_FnAST f            -> [fnBody f]
+            E_SeqAST        a b  -> unbuildSeqs e
+            E_SubscriptAST  a b  -> [a, b]
+            E_TupleAST     es b  -> es
+            E_VarAST       mt v  -> []
+
+instance Expr ExprAST where
+    freeVars e = case e of
+        E_VarAST mt nm      -> [nm]
+        E_FnAST f           -> let bodyvars =  Set.fromList (freeVars (fnBody f)) in
+                               let boundvars = Set.fromList (map (identPrefix.avarIdent) (prototypeASTformals (fnProto f))) in
+                               Set.toList (Set.difference bodyvars boundvars)
+        _                   -> concatMap freeVars (childrenOf e)
+
+
+instance Structured AnnExpr where
+    textOf e width =
+        let spaces = Prelude.replicate width '\SP'  in
+        case e of
+            AnnBool         b    -> out $ "AnnBool      " ++ (show b)
+            AnnCall  r t b a     -> out $ "AnnCall      " ++ " :: " ++ show t
+            AnnCompiles c msg    -> out $ "AnnCompiles  " ++ show c ++ " - " ++ msg
+            AnnIf      t  a b c  -> out $ "AnnIf        " ++ " :: " ++ show t
+            AnnInt ty int        -> out $ "AnnInt       " ++ (litIntText int) ++ " :: " ++ show ty
+            E_AnnFn annFn        -> out $ "AnnFn " ++ fnNameA annFn ++ " // " ++ (show $ annFnBoundNames annFn)
+            AnnSeq          a b  -> out $ "AnnSeq       " ++ " :: " ++ show (typeAST b)
+            AnnSubscript  t a b  -> out $ "AnnSubscript " ++ " :: " ++ show t
+            AnnTuple     es b    -> out $ "AnnTuple     "
+            E_AnnVar (AnnVar t v) -> out $ "AnnVar       " ++ show v ++ " :: " ++ show t
+            E_AnnTyApp t e argty -> out $ "AnnTyApp     [" ++ show argty ++ "] :: " ++ show t
+    childrenOf e =
+        case e of
+            AnnBool         b                    -> []
+            AnnCall  r t b a                     -> [b, a]
+            AnnCompiles c msg                    -> []
+            AnnIf      t  a b c                  -> [a, b, c]
+            AnnInt t _                           -> []
+            E_AnnFn annFn                        -> [annFnBody annFn]
+            AnnSeq      a b                      -> unbuildSeqsA e
+            AnnSubscript t a b                   -> [a, b]
+            AnnTuple     es b                    -> es
+            E_AnnVar      v                      -> []
+            E_AnnTyApp t e argty                 -> [e]
+
+instance Expr AnnExpr where
+    freeVars e = [identPrefix i | i <- freeIdentsA e]
+
+freeIdentsA e = case e of
+        E_AnnVar v      -> [avarIdent v]
+        E_AnnFn f       -> let bodyvars =  freeIdentsA (annFnBody f) in
+                           let boundvars = map avarIdent (annProtoVars (annFnProto f)) in
+                           bodyvars `butnot` boundvars
+        _               -> concatMap freeIdentsA (childrenOf e)
+
+
+annFnBoundNames :: AnnFn -> [String]
+annFnBoundNames fn =
+    let vars = annProtoVars (annFnProto fn) in
+    map show vars
 
