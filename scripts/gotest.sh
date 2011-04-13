46a62f71614d3759d11d3135b1639dd0e5e1e8fb
Ben Karel <eschew@gmail.com>
2011-04-13 14:09:48 -0400

    Update gotest.sh to assume it is running from _obj, and to only run tests if make succeeds.

--- scripts/gotest.sh
+++ scripts/gotest.sh
@@ -2,17 +2,19 @@
 
 P=$1
 shift
-D=../foster/test/$P
+R=../
+D=$R/test/$P
 T=$D/`basename $P`.foster
 
 cleanout () {
   rm -f gclog.txt
 }
 
+echo "testing $D"
 if [ -d $D ]; then
- make fosteroptc fosterparse fosterlower me && cleanout
- echo python ../foster/scripts/run_test.py --verbose ${T} "$@"
- python ../foster/scripts/run_test.py --verbose ${T} "$@"
+ make fosteroptc fosterparse fosterlower me && cleanout && \
+ echo python $R/scripts/run_test.py --verbose ${T} "$@" && \
+ python $R/scripts/run_test.py --verbose ${T} "$@"
 else
   echo "Make new test $T? y/[n]"
   read CONFIRM
