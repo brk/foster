a7cabca592cfb9598100c4d6f081b23fa6a8aadb
Ben Karel <eschew@gmail.com>
2011-04-28 23:50:38 -0400

    Use explicit primitive function values in the interpreter's IR.

--- compiler/me/src/Foster/Smallstep.hs
+++ compiler/me/src/Foster/Smallstep.hs
@@ -1,6 +1,6 @@
 module Foster.Smallstep where
 
-import List(foldr)
+import Data.List as List
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Int
@@ -21,26 +21,31 @@ data SSTerm = SSTmExpr  IExpr
             | SSTmValue SSValue
             deriving (Show)
 
-data SSValue = SSBool     Bool
-             | SSInt      LiteralInt
-             | SSTuple    [SSValue]
-             | SSLocation Location
-             | SSClosure  ILClosure
-             | SSCoro     Coro
+data SSValue = SSBool      Bool
+             | SSInt       LiteralInt
+             | SSPrimitive SSPrimId
+             | SSTuple     [SSValue]
+             | SSLocation  Location
+             | SSClosure   ILClosure
+             | SSCoro      Coro
+             deriving (Show)
+
+data SSPrimId = PrimCoroInvoke | PrimCoroCreate | PrimCoroYield
+              | PrimNamed String
              deriving (Show)
 
 -- Expressions are terms that are not in normal form.
 data IExpr =
           ITuple [AnnVar]
-        | IVar    AnnVar
+        | IVar    Ident
         -- Procedures may be implicitly recursive,
         -- but we need to put a smidgen of effort into
         -- codegen-ing closures so they can be mutually recursive.
         | IClosures    [Ident] [ILClosure] SSTerm
-        | ILetVal       Ident    SSTerm    SSTerm
-        | ISubscript   AnnVar SSTerm
-        | IIf          AnnVar SSTerm SSTerm
-        | ICall        AnnVar [AnnVar]
+        | ILetVal       Ident   SSTerm     SSTerm
+        | ISubscript    Ident   SSTerm
+        | IIf           Ident   SSTerm     SSTerm
+        | ICall         Ident  [Ident]
         | ITyApp       SSTerm  TypeAST
         deriving (Show)
 
@@ -56,13 +61,13 @@ ssTermOfExpr expr =
   case expr of
     ILBool b               -> SSTmValue $ SSBool b
     ILInt t i              -> SSTmValue $ SSInt i
-    ILVar a                -> SSTmExpr  $ IVar a
+    ILVar a                -> SSTmExpr  $ IVar (avarIdent a)
     ILTuple vs             -> SSTmExpr  $ ITuple vs
     ILClosures bnds clos e -> SSTmExpr  $ IClosures bnds clos (tr e)
     ILLetVal x b e         -> SSTmExpr  $ ILetVal x (tr b) (tr e)
-    ILCall    t b vs       -> SSTmExpr  $ ICall b vs
-    ILIf      t  v b c     -> SSTmExpr  $ IIf v (tr b) (tr c)
-    ILSubscript t a b      -> SSTmExpr  $ ISubscript a (tr b)
+    ILCall    t b vs       -> SSTmExpr  $ ICall (avarIdent b) (map avarIdent vs)
+    ILIf      t  v b c     -> SSTmExpr  $ IIf (avarIdent v) (tr b) (tr c)
+    ILSubscript t a b      -> SSTmExpr  $ ISubscript (avarIdent a) (tr b)
     ILTyApp t e argty      -> SSTmExpr  $ ITyApp (tr e) argty
 
 -- ... which lifts in a  straightfoward way to procedure definitions.
@@ -157,8 +162,8 @@ step gs =
 coroFromClosure :: MachineState -> ILClosure -> ((Location, MachineState), Coro)
 coroFromClosure gs (ILClosure id avars) =
   let ssproc = (stProcmap gs) Map.! id in
-  let ins avar map = Map.insert (avarIdent avar) (getval gs avar) map in
-  let env = List.foldr ins Map.empty avars in
+  let ins id map = Map.insert id (getval gs id) map in
+  let env = List.foldr ins Map.empty (map avarIdent avars) in
   let coro = Coro { coroTerm = ssProcBody ssproc
                   , coroArgs = ssProcVars ssproc
                   , coroEnv  = env
@@ -199,23 +204,33 @@ lookupHeap gs loc =
 termOf gs = coroTerm (stCoro gs)
 envOf  gs = coroEnv  (stCoro gs)
 
-isGlobalPrimitive name = case name of
-  "coro_invoke" -> True
-  otherwise -> False
+beginsWith s p = isJust (stripPrefix p s)
 
-getval :: MachineState -> AnnVar -> SSValue
+tryGetVal :: MachineState -> Ident -> Maybe SSValue
+tryGetVal gs id =
+  let mv = Map.lookup id (coroEnv (stCoro gs)) in
+  if isJust mv then mv else
+    case identPrefix id of
+       s | s `beginsWith` "coro_invoke" -> Just $ SSPrimitive PrimCoroInvoke
+       s | s `beginsWith` "coro_create" -> Just $ SSPrimitive PrimCoroCreate
+       s | s `beginsWith` "coro_yield"  -> Just $ SSPrimitive PrimCoroYield
+       s | isPrintFunction  s -> Just $ SSPrimitive (PrimNamed "print")
+       s | isExpectFunction s -> Just $ SSPrimitive (PrimNamed "expect")
+       otherwise -> Nothing
 
--- coro_invoke itself isn't a function that can be represented at the IL level;
--- it must be represented at either the LLVM or Haskell levels.
-getval gs avar | identPrefix (avarIdent avar) == "coro_invoke" =
-        (SSLocation (negate 54321))
 
-getval gs avar = case Map.lookup (avarIdent avar) (coroEnv (stCoro gs)) of
-        Just e -> e
-        Nothing -> error $ "Unable to look up local variable " ++ show avar
+getval :: MachineState -> Ident -> SSValue
+getval gs id =
+  case tryGetVal gs id of
+    Just e -> e
+    Nothing -> error $ "Unable to look up local variable " ++ show id
 
 withTerm gs e = modifyHeapWith gs (stCoroLoc gs) (\(SSCoro c) -> SSCoro $ c { coroTerm = e })
 withEnv  gs e = modifyHeapWith gs (stCoroLoc gs) (\(SSCoro c) -> SSCoro $ c { coroEnv  = e })
+
+callProc gs proc args =
+  let names = map avarIdent (ssProcVars proc) in
+  withTerm (extendEnv gs names args) (ssProcBody proc)
 
 pushCoroCont gs delimCont =
   let currStack = coroStack (stCoro gs) in
@@ -234,7 +249,7 @@ stepExpr gs expr = do
   let coro = stCoro gs
   case expr of
     IVar avar              -> do return $ withTerm gs (SSTmValue $ getval gs avar)
-    ITuple     vs          -> do return $ withTerm gs (SSTmValue $ SSTuple (map (getval gs) vs))
+    ITuple     vs          -> do return $ withTerm gs (SSTmValue $ SSTuple (map (getval gs) (map avarIdent vs)))
     IClosures bnds clos e  ->
       -- This is not quite right; closures should close over each other!
       let clovals = map SSClosure clos in
@@ -249,12 +264,17 @@ stepExpr gs expr = do
 
     ICall b vs ->
         let args = map (getval gs) vs in
-        case Map.lookup (avarIdent b) (stProcmap gs) of
-           Just proc ->
-              let names = map avarIdent (ssProcVars proc) in
-              return $ withTerm (extendEnv gs names args)
-                                (ssProcBody proc)
-           Nothing -> tryEvalPrimitive gs (identPrefix (avarIdent b)) args
+        case Map.lookup b (stProcmap gs) of
+           Just proc -> return (callProc gs proc args)
+           Nothing ->
+             case tryGetVal gs b of
+               Just (SSClosure clo) ->
+                 case Map.lookup (ilClosureProcIdent clo) (stProcmap gs) of
+                   Just proc -> return (callProc gs proc args)
+                   Nothing -> error $ "No proc for closure!"
+               Just (SSPrimitive p) -> evalPrimitive p gs args
+               Just v -> error $ "Cannot call non-closure value " ++ display v
+               Nothing -> tryEvalPrimitive gs (identPrefix b) args
 
     IIf v b c ->
         case getval gs v of
@@ -274,11 +294,8 @@ stepExpr gs expr = do
     ISubscript v (SSTmValue e) ->
       error $ ("step ilsubsc "  ++ show v ++ " // " ++ show e)
 
-    ITyApp e@(SSTmExpr (IVar (AnnVar _ (Ident "coro_invoke" _)))) argty
-      -> return $ withTerm gs e
-
     ITyApp e@(SSTmExpr _) argty -> subStep (withTerm gs e) (envOf gs, \t -> SSTmExpr $ ITyApp t argty)
-
+    ITyApp e@(SSTmValue (SSPrimitive PrimCoroInvoke)) argty -> return $ withTerm gs e
     ITyApp (SSTmValue e) argty -> error $ "step iltyapp " ++ show e
 
 canSwitchToCoro c =
@@ -336,6 +353,90 @@ tryGetInt32PrimOp2Bool name =
     "primitive_>_i32"        -> Just ((>))
     otherwise -> Nothing
 
+--------------------------------------------------------------------
+
+evalPrimitive :: SSPrimId -> MachineState -> [SSValue] -> IO MachineState
+evalPrimitive PrimCoroInvoke gs [(SSLocation targetloc),arg] =
+  let (SSCoro ncoro) = lookupHeap gs targetloc in
+  let ccoro = assert (canSwitchToCoro ncoro) (stCoro gs) in
+  let hadRun = CoroStatusDormant /= coroStat ncoro in
+  let newcoro2 = ncoro { coroPrev = (Just $ coroLoc ccoro)
+                       , coroStat = CoroStatusRunning } in
+  if hadRun
+    then
+       let newcoro = newcoro2 { coroTerm = SSTmValue arg } in
+       return $ gs {
+           stHeap = modifyHeap2 gs (coroLoc ccoro) (SSCoro $ ccoro { coroStat = CoroStatusSuspended })
+                                   (coroLoc ncoro) (SSCoro $ newcoro)
+         , stCoroLoc = coroLoc ncoro
+       }
+    else
+       let newcoro = newcoro2 in
+       let gs2 = gs {
+           stHeap = modifyHeap2 gs (coroLoc ccoro) (SSCoro $ ccoro { coroStat = CoroStatusSuspended })
+                                   (coroLoc ncoro) (SSCoro $ newcoro)
+         , stCoroLoc = coroLoc ncoro
+       } in
+       return $ extendEnv gs2 (tail $ map avarIdent $ coroArgs newcoro) [arg]
+
+evalPrimitive PrimCoroInvoke gs _ = error $ "Wrong arguments to coro_invoke"
+
+
+{- "Rule 4 describes the action of creating a coroutine.
+    It creates a new label to represent the coroutine and extends the
+    store with a mapping from this label to the coroutine main function." -}
+evalPrimitive PrimCoroCreate gs [SSClosure clo] =
+  let ((loc, gs2), coro) = coroFromClosure gs clo in
+  return $ withTerm gs2 (SSTmValue $ SSLocation loc)
+
+evalPrimitive PrimCoroCreate gs _ = error $ "Wrong arguments to coro_create"
+
+
+-- The current coro is returned to the heap, marked suspended, with no previous coro.
+-- The previous coro becomes the new coro, with status running.
+evalPrimitive PrimCoroYield gs [arg] =
+  let ccoro = stCoro gs in
+  case coroPrev ccoro of
+    Nothing -> error $ "Cannot yield from initial coroutine!\n" ++ show ccoro
+    Just prevloc ->
+      let (SSCoro prevcoro) = assert (prevloc /= coroLoc ccoro) $
+                               lookupHeap gs prevloc in
+      let newpcoro = prevcoro { coroStat = CoroStatusRunning
+                              , coroTerm = SSTmValue arg } in
+      let gs2 = assert (canSwitchToCoro prevcoro) $
+                 gs {
+          stHeap = modifyHeap2 gs prevloc (SSCoro newpcoro)
+                          (coroLoc ccoro) (SSCoro $ ccoro { coroPrev = Nothing
+                                                          , coroStat = CoroStatusSuspended })
+        , stCoroLoc = prevloc
+      } in
+      return $ gs2
+evalPrimitive PrimCoroYield gs _ = error $ "Wrong arguments to coro_yield"
+
+
+evalPrimitive (PrimNamed "print") gs [val] =
+      do printString gs (display val)
+         return $ withTerm gs (SSTmValue val)
+
+evalPrimitive (PrimNamed "expect") gs [val] =
+      do expectString gs (display val)
+         return $ withTerm gs (SSTmValue val)
+
+evalPrimitive (PrimNamed p) gs args =
+  error $ "step evalPrimitive named " ++ p ++ " with " ++ show (map display args)
+
+--------------------------------------------------------------------
+
+printString  gs s = do
+  runOutput (outCSLn Blue s)
+  appendFile (errFile gs) (s ++ "\n")
+
+expectString gs s = do
+  runOutput (outCSLn Green s)
+  appendFile (outFile gs) (s ++ "\n")
+
+--------------------------------------------------------------------
+
 tryEvalPrimitive :: MachineState -> String -> [SSValue] -> IO MachineState
 tryEvalPrimitive gs primName [SSInt i1, SSInt i2]
         | isJust (tryGetInt32PrimOp2Int32 primName) =
@@ -358,55 +459,6 @@ tryEvalPrimitive gs "primitive_bitnot_i1" [SSBool b] =
 tryEvalPrimitive gs "force_gc_for_debugging_purposes" _args =
   return $ withTerm gs (SSTmValue $ SSTuple [])
 
-{- "Rule 4 describes the action of creating a coroutine.
-    It creates a new label to represent the coroutine and extends the
-    store with a mapping from this label to the coroutine main function." -}
-tryEvalPrimitive gs ("coro_create_i32_i32") [SSClosure clo] =
-  let ((loc, gs2), coro) = coroFromClosure gs clo in
-  return $ withTerm gs2 (SSTmValue $ SSLocation loc)
-
--- The current coro is returned to the heap, marked suspended, with no previous coro.
--- The previous coro becomes the new coro, with status running.
-tryEvalPrimitive gs ("coro_yield_i32_i32") [arg] =
-  let ccoro = stCoro gs in
-  case coroPrev ccoro of
-    Nothing -> error $ "Cannot yield from initial coroutine!\n" ++ show ccoro
-    Just prevloc ->
-      let (SSCoro prevcoro) = assert (prevloc /= coroLoc ccoro) $
-                               lookupHeap gs prevloc in
-      let newpcoro = prevcoro { coroStat = CoroStatusRunning
-                              , coroTerm = SSTmValue arg } in
-      let gs2 = assert (canSwitchToCoro prevcoro) $
-                 gs {
-          stHeap = modifyHeap2 gs prevloc (SSCoro newpcoro)
-                          (coroLoc ccoro) (SSCoro $ ccoro { coroPrev = Nothing
-                                                          , coroStat = CoroStatusSuspended })
-        , stCoroLoc = prevloc
-      } in
-      do --putStrLn $ "yielding from coro; arg " ++ display arg ++ " replaced " ++ show (coroTerm prevcoro)
-         --putStrLn $ "yielding from coro loc " ++ show (coroLoc ccoro) ++
-         --            " to " ++ show prevloc ++ "\nafter exec:" ++ (show $ coroTerm ccoro)
-         --                                   ++ "\nwill exec :" ++ (show $ coroTerm newpcoro)
-         return $ gs2
-
-tryEvalPrimitive gs ("appty_coro_invoke") ((SSLocation targetloc):args) =
- do --putStrLn $ "invoking coro, passing " ++ show (map display args)
-    return $ tryInvokeToCoro gs targetloc args
-
-tryEvalPrimitive gs ("coro_invoke_i32_i32") ((SSLocation targetloc):args) =
- do --putStrLn $ "invoking coro, passing " ++ show (map display args)
-    return $ tryInvokeToCoro gs targetloc args
-
-tryEvalPrimitive gs primName [val]
-  | isPrintFunction primName =
-      do printString gs (display val)
-         return $ withTerm gs (SSTmValue val)
-
-tryEvalPrimitive gs primName [val]
-  | isExpectFunction primName =
-      do expectString gs (display val)
-         return $ withTerm gs (SSTmValue val)
-
 tryEvalPrimitive gs "opaquely_i32" [val] =
   return $ withTerm gs (SSTmValue val)
 
@@ -419,38 +471,9 @@ tryEvalPrimitive gs "print_i32b" [val@(SSInt i)] =
          return $ withTerm gs (SSTmValue val)
 
 tryEvalPrimitive gs primName args =
-      error ("step ilcall " ++ show primName ++ " with args: " ++ show args)
+      error ("step ilcall 'prim' " ++ show primName ++ " with args: " ++ show args)
 
-printString  gs s = do
-  runOutput (outCSLn Blue s)
-  appendFile (errFile gs) (s ++ "\n")
-
-expectString gs s = do
-  runOutput (outCSLn Green s)
-  appendFile (outFile gs) (s ++ "\n")
-
-tryInvokeToCoro gs targetloc [arg] =
-  let (SSCoro ncoro) = lookupHeap gs targetloc in
-  let ccoro = assert (canSwitchToCoro ncoro) (stCoro gs) in
-  let hadRun = CoroStatusDormant /= coroStat ncoro in
-  let newcoro2 = ncoro { coroPrev = (Just $ coroLoc ccoro)
-                       , coroStat = CoroStatusRunning } in
-  if hadRun
-    then
-       let newcoro = newcoro2 { coroTerm = SSTmValue arg } in
-       gs {
-           stHeap = modifyHeap2 gs (coroLoc ccoro) (SSCoro $ ccoro { coroStat = CoroStatusSuspended })
-                                   (coroLoc ncoro) (SSCoro $ newcoro)
-         , stCoroLoc = coroLoc ncoro
-       }
-    else
-       let newcoro = newcoro2 in
-       let gs2 = gs {
-           stHeap = modifyHeap2 gs (coroLoc ccoro) (SSCoro $ ccoro { coroStat = CoroStatusSuspended })
-                                   (coroLoc ncoro) (SSCoro $ newcoro)
-         , stCoroLoc = coroLoc ncoro
-       } in
-       extendEnv gs2 (tail $ map avarIdent $ coroArgs newcoro) [arg]
+--------------------------------------------------------------------
 
 showBits32 n =
   let bits = map (testBit n) [0 .. (32 - 1)] in
@@ -475,15 +498,16 @@ view :: SSTerm -> String
 view (SSTmValue v) = display v
 view (SSTmExpr (ILetVal x t e)) = "let " ++ show x ++ " = " ++ view t ++ " in\n" ++ view e
 view (SSTmExpr (IIf v a b)) = "if " ++ show v ++ " then { " ++ view a ++ " } else { " ++ view b ++ " }"
-view (SSTmExpr (ICall b vs)) = show (avarIdent b) ++ show (map avarIdent vs)
+view (SSTmExpr (ICall b vs)) = show b ++ show vs
 view (SSTmExpr e) = show e
 
 display :: SSValue -> String
-display (SSBool True ) = "true"
-display (SSBool False) = "false"
-display (SSInt i     ) = show (litIntValue i)
-display (SSTuple vals) = "(" ++ joinWith ", " (map display vals) ++ ")"
-display (SSLocation z) = "<location " ++ show z ++ ">"
-display (SSClosure c ) = "<closure>"
-display (SSCoro    c ) = "<coro>"
+display (SSBool True )  = "true"
+display (SSBool False)  = "false"
+display (SSPrimitive p) = "<" ++ show p ++ ">"
+display (SSInt i     )  = show (litIntValue i)
+display (SSTuple vals)  = "(" ++ joinWith ", " (map display vals) ++ ")"
+display (SSLocation z)  = "<location " ++ show z ++ ">"
+display (SSClosure c )  = "<closure>"
+display (SSCoro    c )  = "<coro>"
 
