1c26e56cc0baee7d8fa75f065b818ef005cec0b9
eschew <eschew@gmail.com>
2011-06-18 19:26:00 +0000

    Fix typo.

--- compiler/passes/DumpToProtobuf.cpp
+++ compiler/passes/DumpToProtobuf.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 Fen Karel. All rights reserved.
+// Copyright (c) 2010 Ben Karel. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE.txt file or at http://eschew.org/txt/bsd.txt
 
