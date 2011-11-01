36176009fa898913aa56ef93967899d3a9a9b116
Ben Karel <eschew@gmail.com>
2011-11-01 11:09:56 -0400

    Clean up and simplify the code in Context.hs a bit.

--- compiler/me/src/Foster/Context.hs
+++ compiler/me/src/Foster/Context.hs
@@ -1,6 +1,6 @@
 module Foster.Context where
 
-import Data.IORef(IORef,newIORef,readIORef,writeIORef)
+import Data.IORef(IORef,newIORef,readIORef,writeIORef,modifyIORef)
 import Data.Map(Map)
 
 import qualified Data.Text as T
@@ -56,37 +56,37 @@ newtype Tc a = Tc (TcEnv -> IO (OutputOr a))
 unTc :: TcEnv -> Tc a    -> IO (OutputOr a)
 unTc e (Tc f) =   f e
 
+retOK :: a -> IO (OutputOr a)
+retOK x = return (OK x)
+
 instance Monad Tc where
-    return x = Tc (\_env -> return (OK x))
-    fail err = Tc (\_env -> return (Errors [outLn err]))
-    m >>= k = Tc (\env -> do { result <- unTc env m
-                             ; case result of
-                                OK expr -> unTc env (k expr)
-                                Errors ss -> return (Errors ss)
-                             })
+    return x = Tc $ \_env -> return (OK x)
+    fail err = Tc $ \_env -> return (Errors [outLn err])
+    m >>= k  = Tc $ \ env -> do result <- unTc env m
+                                case result of
+                                  OK expr -> unTc env (k expr)
+                                  Errors ss -> return (Errors ss)
 
 -- | Given a Tc function and the result of a previous Tc action,
 -- | fmap the function in OutputOr (and return a monadic value).
 tcInject :: (a -> Tc b) -> (OutputOr a) -> Tc b
-tcInject k result = Tc (\env -> do case result of
-                                     OK expr -> unTc env (k expr)
-                                     Errors ss -> return (Errors ss)
-                       )
+tcInject k result = Tc $ \env -> do case result of
+                                      OK expr -> unTc env (k expr)
+                                      Errors ss -> return (Errors ss)
 
 -- Modifies the standard Tc monad bind operator
 -- to append an error message, if necessary.
 tcOnError Nothing  m k = m >>= k
-tcOnError (Just o) m k = Tc (\env -> do { result <- unTc env m
-                                        ; case result of
+tcOnError (Just o) m k = Tc $ \env -> do result <- unTc env m
+                                         case result of
                                            OK expr -> unTc env (k expr)
                                            Errors ss -> return (Errors (o:ss))
-                                        })
 
 tcLift :: IO a -> Tc a
-tcLift action = Tc (\_env -> do { r <- action; return (OK r) })
+tcLift action = Tc $ \_env -> action >>= retOK
 
 tcFails :: [Output] -> Tc a
-tcFails errs = Tc (\_env -> return $ Errors errs)
+tcFails errs = Tc $ \_env -> return $ Errors errs
 
 newTcRef :: a -> Tc (IORef a)
 newTcRef v = tcLift $ newIORef v
@@ -108,24 +108,26 @@ newTcUnificationVar desc = do
     u   <- newTcUniq
     ref <- newTcRef Nothing
     tcRecordUnificationVar (Meta u ref desc)
+      where
+        -- The typechecking environment maintains a list of all the unification
+        -- variables created, for introspection/debugging/statistics wankery.
+        tcRecordUnificationVar :: MetaTyVar -> Tc MetaTyVar
+        tcRecordUnificationVar m = Tc $ \env ->
+                        do modifyIORef (tcUnificationVars env) (m:); retOK m
 
-tcRecordUnificationVar :: MetaTyVar -> Tc MetaTyVar
-tcRecordUnificationVar m
-    = Tc (\env -> do let vr = tcUnificationVars env
-                     vs <- readIORef vr
-                     writeIORef vr (m:vs)
-                     return (OK m))
-
+-- Runs the given action with the given expression added to the "call stack";
+-- this is used to keep track of the path to the current expression during
+-- type checking.
 tcWithScope :: ExprAST -> Tc a -> Tc a
 tcWithScope expr (Tc f)
-    = Tc (\env -> f (env { tcParents = expr:(tcParents env) }))
+    = Tc $ \env -> f (env { tcParents = expr:(tcParents env) })
 
 newTcUniq :: Tc Uniq
-newTcUniq = Tc (\tcenv -> do { let ref = tcEnvUniqs tcenv
-                             ; uniq <- readIORef ref
-                             ; writeIORef ref (uniq + 1)
-                             ; return (OK uniq)
-                             })
+newTcUniq = Tc $ \env -> do let ref = tcEnvUniqs env
+                            uniq <- readIORef ref
+                            writeIORef ref (uniq + 1)
+                            retOK uniq
+
 
 tcFreshT t = newTcUniq >>= (\u -> return (Ident t u))
 
@@ -133,16 +135,13 @@ tcFresh :: String -> Tc Ident
 tcFresh s = tcFreshT (T.pack s)
 
 tcGetCurrentHistory :: Tc [ExprAST]
-tcGetCurrentHistory = Tc (\tcenv -> do { return (OK $
-                                          Prelude.reverse $ tcParents tcenv) })
+tcGetCurrentHistory = Tc $ \env -> do retOK $ Prelude.reverse $ tcParents env
 
-tcGetCtorInfo = Tc (\tcenv -> do { return (OK $ tcCtorInfo tcenv) })
+tcGetCtorInfo       = Tc $ \env -> do retOK $ tcCtorInfo env
 
+-- The type says it all: run a Tc action, and capture any errors explicitly.
 tcIntrospect :: Tc a -> Tc (OutputOr a)
-tcIntrospect action =
-    Tc (\env -> do { result <- unTc env action
-                   ; return (OK result)
-                   })
+tcIntrospect action = Tc $ \env -> do unTc env action >>= retOK
 
 isOK :: OutputOr ty -> Bool
 isOK (OK _) = True
