892a9671444105ec5fbc3783275408e2fcf29c15
Ben Karel <eschew@gmail.com>
2016-01-27 14:50:50 -0500

    Remove vestigial traces of libcpuid.

--- scripts/fosterc.py
+++ scripts/fosterc.py
@@ -49,7 +49,7 @@ def shared(lib):
 def get_static_libs():
     return ' '.join([os.path.join(nativelib_dir(), lib) for lib in
          ("libfoster_main.o " + shared("libchromium_base") + " " +
-          "libcpuid.a libcoro.a libcycle.a").split(" ")])
+          "libcoro.a libcycle.a").split(" ")])
 
 def get_link_flags():
   common = ['-lpthread']
