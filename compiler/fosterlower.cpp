0af9802ea73069ddee3fdadbfe81546543326680
Ben Karel <eschew@gmail.com>
2016-08-26 16:53:29 -0400

    Have fosterlower validate the right path.

--- compiler/fosterlower.cpp
+++ compiler/fosterlower.cpp
@@ -310,7 +310,7 @@ int main(int argc, char** argv) {
   cl::ParseCommandLineOptions(argc, argv, "Bootstrap Foster compiler backend\n");
 
   foster::gPrintLLVMImports = optPrintLLVMImports;
-  foster::validateInputFile(optInputPath);
+  foster::validateInputFile(optInputPath + ".cb");
 
   foster::ensureDirectoryExists(outdirFile(""));
 
