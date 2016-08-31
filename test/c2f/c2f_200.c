5472c9fcd0ac317846e591151bd631a53a6c7db5
Ben Karel <eschew@gmail.com>
2016-08-31 11:18:13 -0400

    [c2f] Add some testcases.

---
+++ test/c2f/c2f_200.c
@@ -0,0 +1,15 @@
+extern int printf (const char *format, ...);
+extern int getchar(void);
+
+int main(void)
+{
+  int n=0;
+  const char* s = "hello";
+loop: if(s[n])
+      {
+        n++;
+        goto loop;
+      }
+  printf("%d\n",n);
+  return 0;
+}
