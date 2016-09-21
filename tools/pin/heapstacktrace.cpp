dcf378211438ac962122e393e7f57ff6fdeb5da8
Ben Karel <eschew@gmail.com>
2016-09-20 21:05:43 -0400

    [pin] Checkpoint.

--- tools/pin/heapstacktrace.cpp
+++ tools/pin/heapstacktrace.cpp
@@ -55,6 +55,7 @@ namespace LEVEL_PINCLIENT { extern VOID RTN_InsertFillBuffer(RTN rtn, IPOINT act
 std::ofstream TraceFile;
 
 UINT64 tsc0;
+UINT64 gNumCalls;
 
 struct alloc_record {
   UINT64 tsc;
@@ -64,6 +65,21 @@ struct alloc_record {
 
 BUFFER_ID bufid;
 PIN_LOCK lock;
+
+// Assuming tsc = cycles, a 2 GHz processor generates 41 tsc bits per second.
+//  In practice, it's maybe 10 million "events" per second (33 bps, 112 MB * 1.3)
+// So shifting off 12 bits means 29 bits per second; about 2 microsecond resolution.
+//  In practice, 150 K events/s (27 bps, 34 MB * 1.3)
+//    With histogram encoding:
+// Shifting off 16 bits would be 25 bits per second, about 64 microsecond resolution.
+//  Estimate: 25 bps, 9 MB * 1.3
+//    Est w/ histogram encoding:
+// Shifting off 20 bits would be 21 bits per second, about 500 microsecond resolution.
+UINT64 tsc_trim(UINT64 t) { return t >> 12; }
+
+// TODO histogram encoding:
+// Bucket StartTimeStamp EndTimeStamp
+//    [Event Size Count]
 
 /* ===================================================================== */
 /* Commandline Switches */
@@ -76,26 +92,26 @@ KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
 
 VOID AllocPinHook_1(UINT64 tsc, ADDRINT size) {
     PIN_GetLock(&lock, 1);
-    TraceFile << dec << 1 << " "  << size << " " << hex << (tsc - tsc0) << endl;
+    TraceFile << dec << 1 << " " << hex << tsc_trim(tsc - tsc0) << " " << dec << size << endl;
     PIN_ReleaseLock(&lock);
 }
 
 VOID AllocPinHook_2(UINT64 tsc, ADDRINT size) {
     PIN_GetLock(&lock, 1);
-    TraceFile << dec << 2 << " "  << size << " " << hex << (tsc - tsc0) << endl;
+    TraceFile << dec << 2 << " " << hex << tsc_trim(tsc - tsc0) << " " << dec << size << endl;
     PIN_ReleaseLock(&lock);
 }
 
 VOID AllocPinHook_3(UINT64 tsc, ADDRINT size) {
     PIN_GetLock(&lock, 1);
-    TraceFile << dec << 3 << " "  << size << " " << hex << (tsc - tsc0) << endl;
+    TraceFile << dec << 3 << " " << hex << tsc_trim(tsc - tsc0) << " " << dec << size << endl;
     PIN_ReleaseLock(&lock);
 }
 
 
 VOID BeforeMain(UINT64 tsc) {
     PIN_GetLock(&lock, 1);
-    TraceFile << dec << 5 << " "  << 0 << " " << hex << (tsc - tsc0) << endl;
+    TraceFile << dec << 5 << " " << hex << tsc_trim(tsc - tsc0) << " " << 0 << endl;
     PIN_ReleaseLock(&lock);
 }
 
@@ -117,12 +133,29 @@ VOID BeforeMain(UINT64 tsc) {
 VOID * BufferFull(BUFFER_ID id, THREADID tid, const CONTEXT *ctxt, VOID *buf,
                   UINT64 numElements, VOID *v)
 {
+    UINT64 tsc_begin = xed_get_time();
+
     PIN_GetLock(&lock, 1);
 
     struct alloc_record* rc =(struct alloc_record*)buf;
-    for(UINT64 i=0; i<numElements; i++){
-      TraceFile << dec << rc[i].typ << " " << rc[i].size << " " << hex << (rc[i].tsc - tsc0) << endl;
+
+    UINT64 prev_tsc_d_trimmed = 0;
+
+    TraceFile << endl << "A " << hex << rc[0].tsc;
+    for(UINT64 i = 0; i < numElements; i++) {
+      UINT64 tsc_d = (rc[i].tsc - tsc0);
+      UINT64 tsc_d_trimmed = tsc_trim(tsc_d);
+      if (tsc_d_trimmed != prev_tsc_d_trimmed) {
+        prev_tsc_d_trimmed = tsc_d_trimmed;
+        TraceFile << endl << "B " << hex << tsc_d_trimmed;
+      }
+
+      TraceFile << " " << dec << rc[i].typ << " " << rc[i].size;
     }
+
+
+    UINT64 tsc_end = xed_get_time();
+    TraceFile << endl << "BufferFull " << hex << tsc_trim(tsc_end - tsc_begin) << endl;
 
     PIN_ReleaseLock(&lock);
     return buf;
@@ -184,11 +217,16 @@ VOID Image(IMG img, VOID *v)
     }
 }
 
-VOID HandleStackPointerMunge(INS ins) {
+VOID PIN_FAST_ANALYSIS_CALL do_incr_by(UINT64* counter, UINT64 amount)
+{
+    (*counter) += amount;
+}
+
+int HandleStackPointerMunge(INS ins) {
   if (INS_IsSub(ins)) {
     if (INS_OperandIsImmediate(ins, 1)) {
-      if (INS_OperandImmediate(ins, 1) == 8016) {
-        TraceFile << "X " << RTN_Name(INS_Rtn(ins)) << endl;
+      if (INS_OperandImmediate(ins, 1) > 516) {
+        TraceFile << endl << "X " << RTN_Name(INS_Rtn(ins)) << " " << dec << INS_OperandImmediate(ins, 1) << endl;
       } else if (INS_OperandImmediate(ins, 1) > 32000) {
         // Ignore very large stack munging
       } else {
@@ -219,46 +257,66 @@ VOID HandleStackPointerMunge(INS ins) {
       }
     }
   } else if (INS_Opcode(ins) == XED_ICLASS_AND
-      ||   INS_Opcode(ins) == XED_ICLASS_LEA
-      ||   INS_Opcode(ins) == XED_ICLASS_MOV) {
+        ||   INS_Opcode(ins) == XED_ICLASS_LEA
+        ||   INS_Opcode(ins) == XED_ICLASS_MOV) {
     /// do nothing
   } else if (INS_Opcode(ins) == XED_ICLASS_PUSH) {
-    // skip 'push ebp'
-    if (INS_OperandIsReg(ins, 0) && INS_OperandReg(ins, 0) == REG_EBP) {
-      // do nothing
+    if (INS_OperandIsReg(ins, 0) && INS_OperandReg(ins, 0) == REG_GBP) {
+      // skip 'push ebp'
     } else {
-      INS_InsertFillBuffer(ins, IPOINT_BEFORE, bufid,
-          IARG_TSC, offsetof(struct alloc_record, tsc),
-          IARG_ADDRINT, 8,
-          offsetof(struct alloc_record, size),
-          IARG_UINT32, 6, offsetof(struct alloc_record, typ),
-          IARG_END);
+      return 1;
     }
   } else {
     //xed_decoded_inst_t* xdi = INS_XedDec(ins);
     //TraceFile << "non-sub/push insn writing stack ptr, opcode is " << INS_Opcode(ins)
     //		  << " with mnemonic " << INS_Mnemonic(ins) << "; nops = " << nops << endl;
   }
-
+  return 0;
 }
 
 // Pin calls this function every time a new rtn is executed
-VOID Insn(INS ins, VOID *v)
+VOID Trace(TRACE tr, VOID *v)
 {
-  if (INS_RegW(ins, 0) == REG_STACK_PTR) {
-    HandleStackPointerMunge(ins);
-  } else if (INS_RegW(ins, 0) == REG_R15) {
-    // OCaml open-coded allocation(?)
-    if (INS_Opcode(ins) == XED_ICLASS_SUB) {
-      if (INS_OperandIsImmediate(ins, 1)) {
-        INS_InsertFillBuffer(ins, IPOINT_BEFORE, bufid,
-            IARG_TSC, offsetof(struct alloc_record, tsc),
-            IARG_ADDRINT,  INS_OperandImmediate(ins, 1),
-            offsetof(struct alloc_record, size),
-            IARG_UINT32, 9, offsetof(struct alloc_record, typ),
-            IARG_END);
+  int numStackPushes = 0;
+  UINT64 numCalls = 0;
+  for (BBL bbl = TRACE_BblHead(tr); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+    for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
+      if (INS_IsCall(ins)) numCalls++;
+
+      if (INS_RegW(ins, 0) == REG_STACK_PTR) {
+        numStackPushes += HandleStackPointerMunge(ins);
+      } else if (INS_RegW(ins, 0) == REG_R15) {
+        // OCaml open-coded allocation(?)
+        if (INS_Opcode(ins) == XED_ICLASS_SUB) {
+          if (INS_OperandIsImmediate(ins, 1)) {
+            INS_InsertFillBuffer(ins, IPOINT_BEFORE, bufid,
+                IARG_TSC, offsetof(struct alloc_record, tsc),
+                IARG_ADDRINT,  INS_OperandImmediate(ins, 1),
+                offsetof(struct alloc_record, size),
+                IARG_UINT32, 9, offsetof(struct alloc_record, typ),
+                IARG_END);
+          }
+        }
       }
     }
+  }
+
+  if (numStackPushes > 0) {
+    INS ins = BBL_InsHead(TRACE_BblHead(tr));
+    INS_InsertFillBuffer(ins, IPOINT_BEFORE, bufid,
+        IARG_TSC, offsetof(struct alloc_record, tsc),
+        IARG_ADDRINT, 8 * numStackPushes,
+        offsetof(struct alloc_record, size),
+        IARG_UINT32, 6, offsetof(struct alloc_record, typ),
+        IARG_END);
+  }
+
+  if (numCalls > 0) {
+    INS ins = BBL_InsHead(TRACE_BblHead(tr));
+    INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(do_incr_by), IARG_FAST_ANALYSIS_CALL,
+                        IARG_PTR, &gNumCalls,
+                        IARG_UINT64, numCalls,
+                        IARG_END);
   }
 }
 
@@ -267,6 +325,7 @@ VOID Insn(INS ins, VOID *v)
 VOID Fini(INT32 code, VOID *v)
 {
     PIN_GetLock(&lock, 1);
+    TraceFile << endl << "C " << dec << gNumCalls << endl;
     TraceFile.close();
     PIN_ReleaseLock(&lock);
 }
@@ -310,14 +369,15 @@ int main(int argc, char *argv[])
         cerr << "Error allocating initial buffer" << endl;
         return 1;
     }
-    
+
+    gNumCalls = 0;
     tsc0 = xed_get_time();
 
     TraceFile << "T " << hex << tsc0 << endl;
 
     // Register Image to be called to instrument functions.
     IMG_AddInstrumentFunction(Image, 0);
-    INS_AddInstrumentFunction(Insn, 0);
+    TRACE_AddInstrumentFunction(Trace, 0);
 
     PIN_AddFiniFunction(Fini, 0);
 
