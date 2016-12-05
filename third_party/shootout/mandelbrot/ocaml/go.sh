29878f85748fef18e9fce8af2c61ee7c6c0dcb89
Ben Karel <eschew@gmail.com>
2016-12-05 00:56:45 -0500

    Add mandelbrot benchmark, with variations.

---
+++ third_party/shootout/mandelbrot/ocaml/go.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+VAR=4.03.0
+
+function doit {
+
+  opam switch ${VAR}
+  eval `opam config env`
+
+  ocamlopt mandelbrot_firstorder.ml     -o mandelbrot_firstorder_O1_${VAR}.exe
+  rm *.o *.cmi *.cmx
+  ocamlopt mandelbrot_firstorder.ml -O2 -o mandelbrot_firstorder_O2_${VAR}.exe
+  rm *.o *.cmi *.cmx
+  ocamlopt mandelbrot_firstorder.ml -O3 -o mandelbrot_firstorder_O3_${VAR}.exe
+  rm *.o *.cmi *.cmx
+  ocamlopt mandelbrot_firstorder.ml -unsafe -O3 -o mandelbrot_firstorder_O3_unsafe_${VAR}.exe
+  rm *.o *.cmi *.cmx
+  ocamlopt mandelbrot_firstorder.ml -unsafe -O3 -fno-PIC -o mandelbrot_firstorder_O3_unsafe_noPIC_${VAR}.exe
+  rm *.o *.cmi *.cmx
+
+  ocamlopt mandelbrot_higherorder.ml -O2 -o mandelbrot_higherorder_O2_${VAR}.exe
+  rm *.o *.cmi *.cmx
+
+  for x in `ls *.exe`; do
+    perf stat ${x} 2048 > /dev/null
+  done
+
+  sha1sum *.exe
+
+  rm *.exe
+
+}
+
+doit
+
+VAR=4.03.0+fp+flambda
+doit
+
