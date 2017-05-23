09879b65186de67c4f8271d49edec53e1612413b
Ben Karel <eschew@gmail.com>
2017-05-23 15:53:21 -0400

    [c2f] Allow our CSmith driver to generate compound & embedded assignments.

--- scripts/csmith-minimal.sh
+++ scripts/csmith-minimal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-~/sw/local/csmith-2.3.0/bin/csmith --no-argc --no-arrays --no-bitfields --no-checksum --no-comma-operators --no-compound-assignment --no-divs --no-embedded-assigns --no-unary-plus-operator --nomain --no-safe-math --no-packed-struct --no-paranoid --no-pointers --no-structs --no-unions --no-volatiles --no-volatile-pointers --no-global-variables --no-builtins --max-array-dim 2 --max-array-len-per-dim 64 --max-block-depth 3 --max-block-size 4 --max-expr-complexity 4 --max-funcs 4 --max-pointer-depth 3 --max-struct-fields 4 --max-union-fields 4
+~/sw/local/csmith-2.3.0/bin/csmith --no-argc --no-arrays --no-bitfields --no-checksum --no-comma-operators --no-divs --nomain --no-safe-math --no-packed-struct --no-paranoid --no-pointers --no-structs --no-unions --no-volatiles --no-volatile-pointers --no-global-variables --no-builtins --max-array-dim 2 --max-array-len-per-dim 64 --max-block-depth 3 --max-block-size 4 --max-expr-complexity 4 --max-funcs 4 --max-pointer-depth 3 --max-struct-fields 4 --max-union-fields 4
 echo '#include <inttypes.h>'
 echo 'int main() { printf("0x%" PRIX64 "\\n", (uint64_t) func_1()); return 0; }'
