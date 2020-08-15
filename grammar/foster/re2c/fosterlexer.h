c53625648d71bf0766d3c0682b11bd7c622090d6
Ben Karel <eschew@gmail.com>
2020-08-15 15:34:05 -0400

    Update to re2c-based lexer and lemon-based parser.

---
+++ grammar/foster/re2c/fosterlexer.h
@@ -0,0 +1,28 @@
+#ifndef FOSTERLEXER_H
+#define FOSTERLEXER_H
+
+typedef struct {
+  int tok;
+  int line;
+  int col;
+  int endline;
+  int endcol;
+} Token;
+
+typedef struct {
+  char* buf;
+  int num_errors;
+
+  int ptr;
+  int cur;
+  int top;
+  int commentdepth;
+  int line;
+  int linepos;
+  int linepos_prev;
+} Scanner;
+
+// Return: 0 for normal tokens; 99 for whitespace or comments.
+int scan_token(Scanner* scanner, Token* t, int buffer_size, unsigned char* yych);
+
+#endif
---
+++ grammar/foster/re2c/fostertokens.h
@@ -0,0 +1,7 @@
+#include "parser.h"
+
+// Pseudo-token types
+#define WHITESPACE -2
+#define LINECOMMENT -3
+#define BLOCKCOMMENT -4
+#define NEWLINE -5
