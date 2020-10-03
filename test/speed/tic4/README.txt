aa07a448eb13e4a2a3d8806f8670e3b2caa41913
Ben Karel <eschew@gmail.com>
2020-10-03 18:21:25 -0400

    Add tic4 files.

---
+++ test/speed/tic4/README.txt
@@ -0,0 +1,10 @@
+https://github.com/stefandd/Tic4/
+
+Ryzen 3600:
+
+  :2020-09-06 02:59:08 ~/foster/test/speed/tic4/ $ luajit tic4.lua bench
+  Analyzed positions: 1255320
+  Evaluating an empty board to depth 7 took 1.836819 secs.
+  :2020-09-06 02:59:27 ~/foster/test/speed/tic4/ $ ./tic4.foster.exe 7
+  Analyzed positions: 1255320
+  Evaluating an empty board to depth 7 took 627.8 ms
---
+++ test/speed/tic4/negamax.lua
@@ -0,0 +1,79 @@
+
+local negaMax = {maxdepth = 4, numsearchpos = 0}
+negaMax.__index = negaMax
+
+function negaMax:evaluate(board, depth)
+    --[[
+    What can be confusing is how the heuristic value of the current node is calculated. In this implementation, this value is always calculated from the point of view of player A, whose color value is one. In other words, higher heuristic values always represent situations more favorable for player A. This is the same behavior as the normal minimax algorithm. The heuristic value is not necessarily the same as a node's return value due to value negation by negamax and the color parameter. The negamax node's return value is a heuristic score from the point of view of the node's current player.
+
+    Negamax scores match minimax scores for nodes where player A is about to play, and where player A is the maximizing player in the minimax equivalent. Negamax always searches for the maximum value for all its nodes. Hence for player B nodes, the minimax score is a negation of its negamax score. Player B is the minimizing player in the minimax equivalent.
+    
+    Variations in negamax implementations may omit the color parameter. In this case, the heuristic evaluation function must return values from the point of view of the node's current player.
+    --]]
+    print ("This function needs to be implemented!")
+end
+
+function negaMax:move_candidates(board, side_to_move)
+    print ("This function needs to be implemented!")
+end
+
+function negaMax:make_move(board, side_to_move, move)
+    print ("This function needs to be implemented!")
+end
+
+function negaMax:negaMax(board, side_to_move, depth, alpha, beta) -- side_to_move: e.g. 1 is blue, -1 is read
+    --
+    -- init vars for root call
+    --
+    if not depth then -- root call 
+        depth = 0
+        alpha = -math.huge
+        beta = math.huge
+        self.numsearchpos = 0 -- reset call counter
+    end
+    --
+    -- test if the node is terminal (i.e. full board or win)
+    --
+    local best_move = -1
+    local score, is_term_node = self:evaluate(board, depth)    
+    -- we abort the recursion if this is a terminal node, or if one of the search abort conditions are met
+    -- 
+    if is_term_node or depth == self.maxdepth then
+        return side_to_move*score, best_move, is_term_node
+    end
+    --
+    -- if not terminal node, eval child nodes
+    --
+    local moves = self:move_candidates(board, side_to_move)
+    score = -math.huge    
+
+    for _, analyzed_move in ipairs(moves) do -- iterate over all boards
+        self.numsearchpos = self.numsearchpos + 1
+        local b = self:make_move(board, side_to_move, analyzed_move)
+        local move_score, _, _ = -self:negaMax(b, -side_to_move, depth+1, -beta, -alpha)
+        if move_score > score then
+            score = move_score
+            best_move = analyzed_move
+        end
+        -- disable alpha-beta pruning
+        --
+        alpha = math.max(alpha, score)
+        if alpha >= beta then
+            break
+        end
+        --
+    end
+    if depth == 0 then
+        --
+        -- exit for root call (depth == 0)
+        --    
+        -- debug stuff
+        --print(string.format("---- Negamax: node info, depth: %d, side: %d, score: %d, best move: %d", depth, side_to_move, score, best_move))
+        --print_board(board)
+        --print(string.format("----"))
+        print("Analyzed positions: " .. self.numsearchpos)
+    end
+    return score, best_move, game_over
+end
+
+return negaMax
