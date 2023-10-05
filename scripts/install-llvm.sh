c07404089316de8678410400b660602df3cf9df0
Ben Karel <eschew@gmail.com>
2023-10-04 19:23:31 -0700

    Update install-llvm.sh script to use whole-repo archives

--- scripts/install-llvm.sh
+++ scripts/install-llvm.sh
@@ -4,8 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE.txt file or at http://eschew.org/txt/bsd.txt
 
-LLVM_VERSION=12.0.0
-LLVM_V=${LLVM_VERSION}.src
+LLVM_VERSION=17.0.1
 LLVM_ROOT=${HOME}/llvm
 
 # $0 is either a path to the script,
@@ -20,42 +19,14 @@ fi
 FOSTER_ROOT=$($SD/normpath.py $SD/..)
 MAKEJ=$(${FOSTER_ROOT}/third_party/vstinner_perf/makej)
 
-# https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/llvm-10.0.0.src.tar.xz
-download_llvm_component () {
-  wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/${1}-${LLVM_V}.tar.xz
-}
-
-
 # invoke from LLVM_ROOT
 checkout_source() {
 pushd src
         echo "downloading sources..."
-        download_llvm_component llvm
-        download_llvm_component clang
-        download_llvm_component compiler-rt
-        download_llvm_component lld
-        download_llvm_component lldb
-        download_llvm_component libcxx
-        download_llvm_component libcxxabi
-        download_llvm_component libunwind
-        #download_llvm_component clang-tools-extra
+        wget https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-${LLVM_VERSION}.tar.gz
 
         echo "unpacking sources..."
-        for proj in $(ls *-${LLVM_V}.tar.xz); do
-          tar -xf ${proj}
-        done
-        if [ ! -d clang-${LLVM_V} ]; then
-          echo "failed to unpack..."
-          exit 1
-        fi
-        mv lld-${LLVM_V}         llvm-${LLVM_V}/tools/lld
-        mv lldb-${LLVM_V}        llvm-${LLVM_V}/tools/lldb
-        mv clang-${LLVM_V}       llvm-${LLVM_V}/tools/clang
-        mv compiler-rt-${LLVM_V} llvm-${LLVM_V}/projects/compiler-rt
-        mv libcxx-${LLVM_V}      llvm-${LLVM_V}/projects/libcxx
-        mv libcxxabi-${LLVM_V}   llvm-${LLVM_V}/projects/libcxxabi
-        mv libunwind-${LLVM_V}   llvm-${LLVM_V}/runtimes/libunwind
-        #mv clang-tools-extra-${LLVM_V} llvm-${LLVM_V}/tools/clang/tools/extra
+        tar -xf llvmorg-${LLVM_VERSION}.tar.gz
 
         echo "done unpacking sources..."
 popd
@@ -63,18 +34,14 @@ popd
 
 # invoke from LLVM_ROOT
 build_source () {
-pushd src/llvm-${LLVM_V}
+pushd src/llvm-project-llvmorg-${LLVM_VERSION}
         echo "building sources..."
 
-        rm -rf build
-        mkdir -p build
-        cd       build
-
-        # Note: it's OK to use GCC 4.x instead of Clang, I think, but using GCC 5 will lead
-        # to pain and suffering until the whole cxx11 abi_tag situation gets worked out.
         #CC=clang CXX=clang++
-        cmake .. -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${LLVM_ROOT}/${LLVM_VERSION} -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLLVM_TARGETS_TO_BUILD="host" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_LINK_LLVM_DYLIB=ON
+        cmake -S llvm -B build -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${LLVM_ROOT}/${LLVM_VERSION} -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLLVM_TARGETS_TO_BUILD="host" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_PROJECTS="clang;lld"
         # On macOS, add: -DDEFAULT_SYSROOT="$(xcrun --show-sdk-path)"
+
+	cd build
 
 	time make -j${MAKEJ}
 
