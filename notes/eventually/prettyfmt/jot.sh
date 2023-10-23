354cc70de3982da71effa49b31543f8a84a7cbd2
Ben Karel <eschew@gmail.com>
2023-10-22 21:21:54 -0700

    Suspended note on autofmt

---
+++ notes/eventually/prettyfmt/jot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Paused experiment to see how we handle a particularly gnarly test case"
+echo "from Bob Nystrom's writeup of dartfmt"
+echo
+
+# https://journal.stuffwithstuff.com/2015/09/08/the-hardest-program-ive-ever-written/
+
+fosterc fryer.foster --me-arg=--fmt --show-cmdlines
